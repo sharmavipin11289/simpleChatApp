573250411b7638e79e83058030e02e378fcbef19
Vipin <sharmavipin11289@gmail.com>
2021-09-30 22:19:48 +0530

    latest

---
+++ Podfile
@@ -0,0 +1,11 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'simpleChat' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for simpleChat
+pod 'Firebase/Analytics'
+
+end
---
+++ simpleChat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  simpleChat
+//
+//  Created by Vipin on 30/09/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
